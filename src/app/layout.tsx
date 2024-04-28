c2657af7b31936df931ebe452fcb171bf0798112
RiadMefti <riadmefti@hotmail.com>
2024-04-27 20:39:34 -0400

    adding theme switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import { ModeToggle } from "@/components/Buttons/ModeToggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModeToggle/>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
